fe4ac2eec11a8cfdc773407f6503a942004a136c
Nooba <dpnguyen261204@gmail.com>
2025-08-20 07:58:58 +0700

    Lab01 begin

---
+++ Lab01/index.js
@@ -0,0 +1,4 @@
+function printHelloWorld(name) {
+    console.log("Hello ".concat(name));
+}
+printHelloWorld("Đặng Phúc Nguyên");
---
+++ Lab01/index.ts
@@ -0,0 +1,4 @@
+function printHelloWorld(name: string): void {
+    console.log(`Hello ${name}`);
+}
+printHelloWorld("Đặng Phúc Nguyên");
