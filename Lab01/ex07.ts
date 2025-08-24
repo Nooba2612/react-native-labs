197faaf5869f2ae32b39d23edd6e8f1a8f00ad6e
Nooba <dpnguyen261204@gmail.com>
2025-08-24 15:07:16 +0700

    Lab 01 Exercise 07

---
+++ Lab01/ex07.ts
@@ -0,0 +1,20 @@
+class User {
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    setName(name: string): void {
+        this.name = name;
+    }
+}
+
+const user = new User("Nooba");
+console.log("User name: ", user.getName());
+user.setName("Dang Phuc Nguyen");
+console.log("User name: ", user.getName());
