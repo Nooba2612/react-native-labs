b34bbf583bb323830ced5548af05c095566a05b7
Nooba <dpnguyen261204@gmail.com>
2025-08-27 07:21:34 +0700

    Lab 02 Exercise 01

---
+++ Lab02/ex01.ts
@@ -0,0 +1,9 @@
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Hello Async");
+    }, 2000);
+});
+
+myPromise.then((result) => {
+    console.log(result);
+});
